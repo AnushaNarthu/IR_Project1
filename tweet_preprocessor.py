ee55b68b2bd8f3932f646bb5e084d7e5b2cda9b3
Anusha Narthu <78515787+AnushaNarthu@users.noreply.github.com>
2021-09-22 00:59:22 -0400

    Update tweet_preprocessor.py

--- tweet_preprocessor.py
+++ tweet_preprocessor.py
@@ -12,13 +12,54 @@ import preprocessor
 
 class TWPreprocessor:
     @classmethod
-    def preprocess(cls, tweet):
+    def preprocess(cls, tweet, poi):
         '''
         Do tweet pre-processing before indexing, make sure all the field data types are in the format as asked in the project doc.
         :param tweet:
         :return: dict
         '''
+        text = None
+        if tweet.in_reply_to_user_id:
+            temp = ''
+            if tweet._json.get('text'):
+                temp = tweet._json.get('text').split(" ")
+            else :
+                temp = tweet._json.get('full_text').split(" ")
+            temp.pop(0)
+            temp = " ".join(temp)
+            text = temp
+        else :
+            text = tweet.full_text
 
+        clean_text, emojis = _text_cleaner(text)
+        data = {'poi_name': tweet.user.screen_name,
+            'poi_id': tweet.user.id,
+            'verified': tweet.user.verified,
+            'country': poi['country'],
+            'replied_to_tweet_id': tweet.in_reply_to_status_id,
+            'replied_to_user_id': tweet.in_reply_to_user_id,
+            'tweet_text': text,
+            'tweet_xx': clean_text,
+            'tweet_lang': tweet.lang,
+            'hashtags': _get_entities(tweet,type ="hashtags"),
+            'mentions': _get_entities(tweet, type ="mentions"),
+            'tweet_urls': _get_entities(tweet,type = "urls"),
+            'tweet_date': tweet.created_at,
+            'tweet_loc': tweet.geo,
+            'retweeted': tweet.retweeted,
+            'replied_to_screen_name': tweet.in_reply_to_screen_name,
+            'tweet_emoticons':emojis,
+            'reply_text': None
+           }
+        if tweet.in_reply_to_user_id:
+            temp1 ={
+            'poi_name': tweet.user.screen_name,
+            'poi_id': tweet.user.id,
+            'reply_text': text
+            }
+            data.update(temp1)
+        #print(data)
+        return data
         raise NotImplementedError
 
 
