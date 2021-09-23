634fb585fcbd2eb6d33e74ef607498deedb71fa3
Anusha Narthu <78515787+AnushaNarthu@users.noreply.github.com>
2021-09-23 00:02:13 -0400

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -20,186 +20,91 @@ class Twitter:
         Add basic tweet requirements logic, like language, country, covid type etc.
         :return: boolean
         '''
-        
-        
         raise NotImplementedError
-
-    def get_tweets_by_poi_screen_name(self, poi_screenname,cnt):
+    def get_tweets_by_poi_screen_name(self, poi_screen_name):
         '''
         Use user_timeline api to fetch POI related tweets, some postprocessing may be required.
         :return: List
         '''
-        #max_tweets=150
-        c = 0
-        d = 0
-        oldest_id = None
-        tweets_data = []
-        while d < 801 :
-            tweets = self.api.user_timeline(id =poi_screenname, count =100, max_id = oldest_id,tweet_mode='extended',)
-            #print(len(tweets))
-            for tweet in tweets:
-                single_tweet = {}
-                d = d+1
+        tweets = []
+        covid_tweets = []
+        key_word_tweets_count = 0
+        max_id = None
+        reply_max_id = None
+        while key_word_tweets_count < 51 or len(tweets) < 600:
+            tweets_temp = self.api.user_timeline(screen_name=poi_screen_name,count=600,include_rts = False,tweet_mode = 'extended',max_id=max_id)
+            max_id = tweets_temp[-1].id
+            for tweet in tweets_temp:
+                #print("Text : ",tweet.full_text)
+                if 'COVID' in tweet.full_text.upper():
+                    key_word_tweets_count +=1
+                    covid_tweets.append(tweet)
+            tweets.extend(tweets_temp)
+            print("collected covid----------------------",key_word_tweets_count)
+        print("Total tweets count -",len(tweets))
+        results = []
+        replies_result = []
+        invalid =0
+        covid_tweets = covid_tweets[0:10]
+        for tweet in covid_tweets:
+            ran = randint(8, 16)  
+            reply_max_id = None
+            replies_count = 0
+            #while replies_count < 11:
+            replies = self.get_replies("to:{}".format(tweet.user.screen_name) , tweet.id)
+            
+            replies_for_current_tweet = []
+            for reply in replies:
+                if reply.in_reply_to_status_id == tweet.id:
+                    replies_for_current_tweet.append(reply)
+                    replies_count +=1
+                else :
+                    invalid +=1
+            replies_result.extend(replies_for_current_tweet)
+            print("replies - ",len(replies)," related -",len(replies_for_current_tweet))
+                #if len(replies) > 0:
+                #    reply_max_id = replies[-1].id
+            if len(replies_result) > 600:
+                break
                 
-                
+        tweets.extend(replies_result)
+        print("Replies count : ",len(replies_result))
+        return tweets
+        #raise NotImplementedError
 
-                single_tweet["id"] = str(tweet.id)
-                single_tweet["verified"] = tweet.user.verified
-                single_tweet["poi_name"] = tweet.user.screen_name
-                single_tweet["poi_id"] = tweet.user.id
-                if tweet.lang == "en":
-                    single_tweet["country"] = "USA"
-                elif tweet.lang  == "hi":
-                    single_tweet["country"] =  "INDIA"
-                else:
-                    single_tweet["country"] = "MEXICO"
-                single_tweet["tweet_text"] = tweet.full_text
-                single_tweet["tweet_lang"] = tweet.lang
-
-
-                x = tweet.entities
-                y = x['hashtags']
-                count_hashtags = len(y)
-                all_hashtags = []
-                if count_hashtags>0:
-                    for hashtag in y:
-                        hash_text = hashtag['text']
-                        all_hashtags.append(hash_text)
-                    single_tweet["hashtags"] = all_hashtags
-                #mentions
-                result_mention = []
-                mentions = x['user_mentions']
-                if len(mentions)>0:
-                    for mention in mentions:
-                        result_mention.append(mention['screen_name'])
-                    single_tweet["mentions"] = result_mention
-                
-                
-                ####
-                #urls
-                result_url = []
-                urls = x['urls']
-                if len(urls)>0:
-                    for url in urls:
-                        result_url.append(url['url'])
-                    single_tweet["urls"] = result_url
-                    
-                
-                
-                ####
-                date_str = parse(str(tweet.created_at))
-                time_obj = date_str.replace(second=0, microsecond=0, minute=0, hour=date_str.hour) + timedelta(
-                hours=date_str.minute // 30)
-                date_str = datetime.datetime.strftime(time_obj, '%Y-%m-%dT%H:%M:%SZ')
-
-                #single_tweet["tweet_date"]= status.created_at.strftime("%y-%m-%dT%H:%M:%SZ")
-                #single_tweet["tweet_date"]= str(status.created_at)
-                single_tweet["tweet_date"] = date_str
-                tweets_data.append(single_tweet)
-
-                if re.search("COVID", tweet.full_text):
-                    c =c+1                   
-
-                tweetid =tweet.id
-            oldest_id = tweetid
-
-        print("covid",c)
-        return tweets_data
-       
-     #  raise NotImplementedError
-
-    def get_tweets_by_lang_and_keyword(self,key_count,key_name,key_lang):
+    def get_tweets_by_lang_and_keyword(self,keyword):
         '''
         Use search api to fetch keywords and language related tweets, use tweepy Cursor.
         :return: List
         '''
+        tweets = []
+        max_id = None
+        while len(tweets) < 300:
+            
+            tweets_temp = self.api.search(q=keyword,count=200,include_rts = False,tweet_mode = 'extended',max_id=max_id)
+            print("1------------",len(tweets_temp))
+            max_id = tweets_temp[-1].id
+            tweets.extend(tweets_temp)
 
-        tweets_data = []
-        
-    
-        for status in tweepy.Cursor(self.api.search,q = key_name, lang = key_lang).items(300):
-            #print(status)
-            #status_dict = dict(vars(status))
-            #keys = status_dict.keys()
-            #for k in keys:
-                #print(k)
-            #    single_tweet[k] = status_dict[k]
-            #user_dict = dict(vars(status_dict['user']))
-            
-            single_tweet = {}
-            single_tweet["id"] = str(status.id)
-            single_tweet["verified"] = status.user.verified
-            if key_lang == "en":
-                single_tweet["country"] = "USA"
-            elif key_lang == "hi":
-                single_tweet["country"] =  "INDIA"
-            else:
-                single_tweet["country"] = "MEXICO"
-            single_tweet["tweet_text"] = status.text
-            single_tweet["tweet_lang"] = key_lang
-
-            
-            
-            x = status.entities
-            y = x['hashtags']
-            count_hashtags = len(y)
-            all_hashtags = []
-            if count_hashtags>0:
-                for hashtag in y:
-                    hash_text = hashtag['text']
-                    all_hashtags.append(hash_text)
-
-            single_tweet["hashtags"] = all_hashtags
-            
-            
-            #mentions
-            result_mention = []
-            mentions = x['user_mentions']
-            if len(mentions)>0:
-                for mention in mentions:
-                    result_mention.append(mention['screen_name'])
-                single_tweet["mentions"] = result_mention
-                
-                
-            ####
-            #urls
-            result_url = []
-            urls = x['urls']
-            if len(urls)>0:
-                for url in urls:
-                    result_url.append(url['url'])
-                single_tweet["urls"] = result_url
-                    
-                
-                
-                ####
-            
-            
-            date_str = parse(str(status.created_at))
-            time_obj = date_str.replace(second=0, microsecond=0, minute=0, hour=date_str.hour) + timedelta(
-            hours=date_str.minute // 30)
-            date_str = datetime.datetime.strftime(time_obj, '%Y-%m-%dT%H:%M:%SZ')
-
-            #single_tweet["tweet_date"]= status.created_at.strftime("%y-%m-%dT%H:%M:%SZ")
-            #single_tweet["tweet_date"]= str(status.created_at)
-            single_tweet["tweet_date"] = date_str
-            tweets_data.append(single_tweet)
-
-
-        return tweets_data
-            
+        results = []
+        replies_result = []
+        invalid = 0
+        tweet_count = 0
+        max_id = None
 
         
-        #print(tweets_data)
+        return tweets
         #raise NotImplementedError
-        
-  
-    def get_replies(self, query, max_id ):
+
+    def get_replies(self,query,since_id):
         '''
         Get replies for a particular tweet_id, use max_id and since_id.
         For more info: https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/guides/working-with-timelines
         :return: List
         '''
-        replies = self.api.search(q= query, since_id =max_id, count =1000)
+        #print(query)
+        replies = self.api.search(q = query, since_id=since_id,count =500)
+        #for reply in replies:
+        #    print(reply,"reply-------------------------------------------------")
         return replies
-        raise NotImplementedError
+        #raise NotImplementedError
