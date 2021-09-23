65c02b1f85acfb9235a366c8e3be6a117589319e
Anusha Narthu <78515787+AnushaNarthu@users.noreply.github.com>
2021-09-23 11:49:06 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -37,25 +37,21 @@ def main():
     
     pois = [{"id": 1, "screen_name": "JoeBiden", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
            {"id": 2, "screen_name": "CDCgov", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-           {"id": 3, "screen_name": "drharshvardhan", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-           {"id": 4, "screen_name": "SenatorLeahy", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-           {"id": 5, "screen_name": "SecBecerra", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+           {"id": 3, "screen_name": "GavinNewsom", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+           {"id": 4, "screen_name": "POTUS", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+           {"id": 5, "screen_name": "sensanders", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
            {"id": 6, "screen_name": "narendramodi", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
            {"id": 7, "screen_name": "AmitShah", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-           {"id": 8, "screen_name": "KTRTRS", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+           {"id": 8, "screen_name": "JPNadda", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
             {"id": 9, "screen_name": "RahulGandhi", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
             {"id": 10, "screen_name": "ArvindKejriwal", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
             {"id":11, "screen_name": "osoriochong", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
             {"id": 12, "screen_name": "FelipeCalderon", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
             {"id": 13, "screen_name": "lopezobrador_", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 14, "screen_name": "SSalud_mx", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 15, "screen_name": "GustavoMadero", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 16, "screen_name": "ManceraMiguelMX", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 17, "screen_name": "GovLarryHogan", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 18, "screen_name": "AyushmanNHA", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 19, "screen_name": "PMOIndia", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 20, "screen_name": "KamalaHarris", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
-            {"id": 21, "screen_name": "MoHFW_INDIA", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0}]
+            {"id": 14, "screen_name": "GustavoMadero", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+            {"id": 15, "screen_name": "ManceraMiguelMX", "country": "MEXICO", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+            {"id": 16, "screen_name": "GovLarryHogan", "country": "USA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0},
+            {"id": 17, "screen_name": "PMOIndia", "country": "INDIA", "count": 1, "finished": 0 , "reply_finished": 0, "collected": 0}]
     #keywords = config["keywords"]
     keywords = [
     {
@@ -210,7 +206,46 @@ def main():
       "country": "MEXICO",
       "finished": 0
     },
-  
+    {
+      "id": 20,
+      "name": "eficacia",
+      "count": 5,
+      "lang": "es",
+      "country": "MEXICO",
+      "finished": 0
+    },
+    {
+      "id": 21,
+      "name": "segunda dosis",
+      "count": 5,
+      "lang": "es",
+      "country": "MEXICO",
+      "finished": 0
+    },
+    {
+      "id": 22,
+      "name": "dosis",
+      "count": 5,
+      "lang": "es",
+      "country": "MEXICO",
+      "finished": 0
+    },
+    {
+      "id": 23,
+      "name": "hospital",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 24,
+      "name": "covidresources",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
     {
       "id": 25,
       "name": "rt-pcr",
@@ -251,7 +286,38 @@ def main():
       "country": "USA",
       "finished": 0
     },
-  
+    {
+      "id": 30,
+      "name": "covidsecondwaveinindia",
+      "count": 5,
+      "lang": "en",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 31,
+      "name": "flattenthecurve",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 32,
+      "name": "stay safe",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 33,
+      "name": "covid test",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
     {
       "id": 34,
       "name": "covid symptoms",
@@ -268,8 +334,150 @@ def main():
       "country": "USA",
       "finished": 0
     },
-    
-   
+    {
+      "id": 36,
+      "name": "disease",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 37,
+      "name": "staysafe",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 38,
+      "name": "doctor",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 39,
+      "name": "facemask",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 40,
+      "name": "epidemic",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 41,
+      "name": "transmission",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 42,
+      "name": "social distance",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 43,
+      "name": "surgical mask",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 44,
+      "name": "vaccines",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 45,
+      "name": "covid vaccine",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 46,
+      "name": "vaccine side effect",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 47,
+      "name": "johnson & johnson",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 48,
+      "name": "immunity",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 49,
+      "name": "vaccination drive",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id":50,
+      "name": "we4vaccine",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 51,
+      "name": "injection",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 52,
+      "name": "sputnik",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
+    {
+      "id": 53,
+      "name": "astrazeneca",
+      "count": 5,
+      "lang": "en",
+      "country": "USA",
+      "finished": 0
+    },
     {
       "id": 54,
       "name": "वैश्विकमहामारी",
@@ -374,7 +582,70 @@ def main():
       "country": "INDIA",
       "finished": 0
     },
-   
+    {
+      "id": 66,
+      "name": "वाइरस",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 67,
+      "name": "वेंटिलेटर",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 68,
+      "name": "डेल्टा",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 69,
+      "name": "मौत",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 70,
+      "name": "कोविड महामारी",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 71,
+      "name": "फ़्लू",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 72,
+      "name": "स्वच्छ",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 73,
+      "name": "लक्षण",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
     {
       "id": 74,
       "name": "कोविड के केस",
@@ -535,23 +806,102 @@ def main():
       "country": "INDIA",
       "finished": 0
     },
-    
+    {
+      "id": 93,
+      "name": "कोवैक्सीन",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 94,
+      "name": "कोवैक्सिन",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 95,
+      "name": "खराब असर",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 96,
+      "name": "कोविड का टीका",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 97,
+      "name": "वाइरस",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 98,
+      "name": "खुराक",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 99,
+      "name": "कोवेक्सिन",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 100,
+      "name": "वैक्सीनेशन",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 101,
+      "name": "एंटीबॉडी",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
+    {
+      "id": 102,
+      "name": "कोविशील्ड",
+      "count": 5,
+      "lang": "hi",
+      "country": "INDIA",
+      "finished": 0
+    },
    
   ]
-    
+    cnt=0
     for i in range(len(pois)):
         if pois[i]["finished"] == 0:
             print(f"---------- collecting tweets for poi: {pois[i]['screen_name']}")
-
-            raw_tweets = twitter.get_tweets_by_poi_screen_name(pois[i]['screen_name'])  # pass args as needed
-
+            raw_tweets = twitter.get_tweets_by_poi_screen_name(pois[i]["screen_name"], pois[i]["count"])
+            
             processed_tweets = []
-            for tw in raw_tweets:
-                tweet_up = TWPreprocessor.preprocess(tw,pois[i],'poi')
-                if tweet_up !=None:
-                    processed_tweets.append(tweet_up)
+            for tw in raw_tweets:                
+                processed_tweets.append(TWPreprocessor.preprocess(tw))
+                cnt = cnt + 1
+                #print(tw)
+                #print(cnt)
 
-            #indexer.create_documents(processed_tweets)
+            indexer.create_documents(processed_tweets)
 
             pois[i]["finished"] = 1
             pois[i]["collected"] = len(processed_tweets)
@@ -562,19 +912,22 @@ def main():
 
             save_file(processed_tweets, f"poi_{pois[i]['id']}.pkl")
             print("------------ process complete -----------------------------------")
-    
+   
+    count = 0
     for i in range(len(keywords)):
         if keywords[i]["finished"] == 0:
             print(f"---------- collecting tweets for keyword: {keywords[i]['name']}")
-
-            raw_tweets = twitter.get_tweets_by_lang_and_keyword(keywords[i]['name'])  # pass args as needed
+            raw_tweets = twitter.get_tweets_by_lang_and_keyword(keywords[i]["count"], keywords[i]["name"], keywords[i]["lang"])
 
             processed_tweets = []
+            
             for tw in raw_tweets:
-                tweet_up = TWPreprocessor.preprocess(tw,pois[i],'keyword')
-                if tweet_up !=None:
-                    processed_tweets.append(tweet_up)
+                processed_tweets.append(TWPreprocessor.preprocess(tw))
+                count = count + 1
+                #print(tw)
+                #print(count)
 
+            
             indexer.create_documents(processed_tweets)
 
             keywords[i]["finished"] = 1
@@ -585,13 +938,12 @@ def main():
             })
 
             save_file(processed_tweets, f"keywords_{keywords[i]['id']}.pkl")
-
+            
             print("------------ process complete -----------------------------------")
 
     if reply_collection_knob:
         # Write a driver logic for reply collection, use the tweets from the data files for which the replies are to collected.
-        pass
-        #raise NotImplementedError
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
